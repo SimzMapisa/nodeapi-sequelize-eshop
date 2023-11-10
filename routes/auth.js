ebd08dc0443b851a7e67e6490ce37f5a1e008ba9
SimzMapisa <simbarashe795@gmail.com>
2023-11-10 13:44:49 +0200

    sperate routes logic to pactice seperation of concerns, yet to add authetication

--- routes/auth.js
+++ routes/auth.js
@@ -1,76 +1,130 @@
 const express = require('express');
 const { Admin, Customer } = require('../models');
-const ROLES = require('../utils/roles');
-const hashPassword = require('../utils/hashpass');
+const { ROLES } = require('../utils/roles');
+const { hashPassword } = require('../utils/hashpass');
 const router = express.Router();
 
-// ****************************  CUSTOMER ROUTES *********************************
+// ****************************  ROUTES *********************************
+/**
+ * @route POST /admins/create_super
+ * @group Admins - Operations about admins
+ * @param {string} name.body.required - admin name
+ * @param {string} email.body.required - admin email
+ * @param {string} password.body.required - admin password
+ */
 
-//create customer
-router.post('/customers/create', async (req, res) => {
-	console.log('Customer creation logic here');
-});
-
-// get all customers
-// Protected route only admins can have this access
-router.get('/customers/all', async (req, res) => {
-	console.log('Get all customers');
-});
-
-// get One customer by ID
-// Protected route only admins can have this access
-router.get('/customers/:uuid', async (req, res) => {
-	console.log('Get one customer');
-});
-
-// update user info
-// Protected route admins and cusomers can edit account info
-router.put('/customers/edit/:uuid', async (req, res) => {});
-
-// Delete a customer account
-// Protected route only admins and customer with that id can delete the account.
-router.delete('/customers/:uuid', async (req, res) => {
-	console.log('Delete customer');
-});
-
-// ****************************  ADMIN ROUTES *********************************
-
-//create customer
-router.post('/admins/create', async (req, res) => {
-	const { name, email, password, role } = req.body;
-
-	console.log('Valid Roles:', Object.values(ROLES));
-	console.log('Provided Role:', role);
+router.post('/admins/create_super', async (req, res) => {
+	const { name, email, password } = req.body;
 
 	try {
 		const checkUser = await Admin.findOne({ where: { email } });
 		if (checkUser) {
-			res.json({ msg: 'Account already exist please login instead!' });
-		} else {
-			const hashedPass = await hashPassword(password);
-
-			const admin = await Admin.create({
-				name,
-				email,
-				password: hashedPass,
-				role,
-			});
-			return res.status(201).json(admin);
+			return res.json({ msg: 'Account already exist please login instead!' });
 		}
+
+		const hashedPass = await hashPassword(password);
+
+		// Check if any admin exists
+		const existingAdmin = await Admin.findOne();
+
+		// If no admin exists, make this user a super admin
+		const role = existingAdmin ? ROLES.ADMIN : ROLES.SUPER_ADMIN;
+
+		const admin = await Admin.create({
+			name,
+			email,
+			password: hashedPass,
+			role,
+		});
+
+		return res.json(admin);
 	} catch (error) {
-		res.json({ error: error.message });
+		return res.status(500).json({ msg: 'An error occurred', error });
+	}
+});
+
+/**
+ * @route POST /admins/create
+ * @description Create a new admin account with role of moderator by default this can be changed later and only super_admins * create new admins
+ */
+
+// TODO: add authentication middleware to this route so only super_admins can create new admins
+
+router.post('/admins/create', async (req, res) => {
+	const { name, email, password, role } = req.body;
+
+	try {
+		const checkUser = await Admin.findOne({ where: { email } });
+		if (checkUser) {
+			return res.json({ msg: 'Account already exist please login instead!' });
+		}
+		const hashedPass = await hashPassword(password);
+		const admin = await Admin.create({
+			name,
+			email,
+			password: hashedPass,
+			role: role || ROLES.MODERATOR,
+		});
+
+		return res.json(admin);
+	} catch (error) {
+		return res.status(500).json({ msg: 'An error occurred', error });
+	}
+});
+
+// here we will create the customer ceate account route
+
+router.post('/customer', async (req, res) => {
+	const { name, email, physicalAddress, password, role } = req.body;
+
+	try {
+		const checkUser = await Customer.findOne({ where: { email } });
+		if (checkUser) {
+			return res.json({ msg: 'Account already exist please login instead!' });
+		}
+		const hashedPass = await hashPassword(password);
+		const customer = await Customer.create({
+			name,
+			email,
+			physicalAddress,
+			password: hashedPass,
+			role: role || ROLES.CUSTOMER,
+		});
+
+		return res.json(customer);
+	} catch (error) {
+		return res.status(500).json({ msg: 'An error occurred', error });
 	}
 });
 
 // Admin Login Route
 router.post('/admins/login', (req, res, next) => {
-	passport.authenticate('local', (err, admin, info));
+	// create the login logic here
+	passport.authenticate('local', (err, user, info) => {
+		if (err) throw err;
+		if (!user) res.send('No user exists');
+		req.logIn(user, (err) => {
+			if (err) throw err;
+			res.send('Successfully Authenticated');
+			console.log(req.user);
+		});
+	});
 });
 
-// get all admins
-// Protected route only admins can have this access
+/**
+ * @route GET /admins/all
+ * @description Get all admins only super_admins, admins and moderators can have this access
+ */
+
+// TODO: add authentication middleware to this route so only super_admins, admins and moderators can have this access
+
 router.get('/admins/all', async (req, res) => {
-	console.log('Get all admins');
+	try {
+		const admins = await Admin.findAll();
+		return res.json(admins);
+	} catch (error) {
+		return res.status(500).json({ msg: 'An error occurred', error });
+	}
 });
 
 // get One customer by ID
@@ -79,8 +133,13 @@ router.get('/admins/:uuid', async (req, res) => {
 	console.log('Get one admin');
 });
 
-// update user info
-// Protected route admins and admins can edit account info
+/**
+ * @route PUT /admins/edit/:uuid
+ * @description Edit a user account and only admins super admins moderator and the user can edit the account
+ */
+
+// TODO: add authentication middleware to this route so only admins super admins moderator and the user can edit the account
+
 router.put('/admins/edit/:uuid', async (req, res) => {});
 
 // Delete a customer account
